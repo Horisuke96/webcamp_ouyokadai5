5d25927e74072c804d0d431c3559e39025533f62
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-06-06 07:17:30 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
 # deviseは先
 
@@ -11,16 +12,13 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
 
-
-
-
   resources :books, only: [:new, :create, :edit, :update, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
 
-
-
   root 'homes#top'
   get '/home/about' => 'homes#about'
+
+  get 'search/search'
 end
